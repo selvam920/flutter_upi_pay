061e102d8175f793eecf485f77084e0ba6a6a991
selvam920 <skssoftware@live.com>
2026-08-23 22:29:09 +0530

    upgrade to material ui package

--- lib/src/meta.dart
+++ lib/src/meta.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter_upi_india/src/applications.dart';
 import 'package:flutter_upi_india/src/platform_utils.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 
 /// Represents a UPI payment app and more relevant information.
 ///
